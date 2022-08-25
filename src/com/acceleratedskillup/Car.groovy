7094bd5f145fb32dbe1a1491bb21ea402c650ecd
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-08-25 22:10:26 +0300

    First

--- src/com/acceleratedskillup/Car.groovy
+++ src/com/acceleratedskillup/Car.groovy
@@ -1,20 +1,20 @@
 package com.acceleratedskillup;
 
 class Car {
-  int numberOfDoors
-  String brand
-  String model
-  def script
-  
-	Car(script) {
-		this.script = script
-	}
-	
-  	void printOutCar() {
-		script.echo "using script.echo"
-  		script.echo "numberOfDoors:${numberOfDoors}"    	
-		script.echo "The car brand is:${brand}"
-		script.echo "The car model is:${model}"
-  	}  
-  
+    int numberOfDoors
+    String brand
+    String model
+    def script
+
+    Car(script) {
+        this.script = script
+    }
+
+    void printOutCar() {
+        script.echo "using script.echo"
+        script.echo "numberOfDoors:${numberOfDoors}"
+        script.echo "The car brand is:${brand}"
+        script.echo "The car model is:${model}"
+    }
+
 }

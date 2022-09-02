be7acda9f9777a5b27fbaf43a4c29c5174ca9db6
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-09-02 16:53:17 +0300

    First

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,7 @@
 def call(Map config = [:]){
     def scriptcontents = libraryResource "com/acceleratedskillup/scripts/linux/${config.name}"
     writeFile file: "${config.name}", text: scriptcontents
+    println scriptcontents
+    println writeFile
     sh "chmod a+x ./${config.name}"
 }

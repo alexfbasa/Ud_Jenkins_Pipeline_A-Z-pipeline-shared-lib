b53975aa76d07575f6e2ccc6e5d5a033621645ed
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-09-02 15:18:24 +0300

    First

--- vars/execBuildOrDeploy.groovy
+++ vars/execBuildOrDeploy.groovy
@@ -1,50 +1,45 @@
 def call(String command) {
-	if (command == "build") 
-	{
-		pipeline {
-			agent any
-			stages 
-			{
-				stage("Build") 
-				{
-					steps 
-					{
-						echo "Build is executed"
-					}
-				}	
-			}
-		}
-	}
-	else if(command == "deploy")
-	{
-		pipeline {
-			agent any
-			stages 
-			{
-				stage("Deploy") 
-				{
-					steps 
-					{
-						echo "Deploy is executed"
-					}
-				}	
-			}
-		}	
-	}
-	else
-	{
-		pipeline {
-			agent any
-			stages 
-			{
-				stage("Do Nothing") 
-				{
-					steps 
-					{
-						echo "Doing nothing"
-					}
-				}	
-			}
-		}		
-	}
+    if (command == "build") {
+        pipeline {
+            agent any
+            stages
+                    {
+                        stage("Build")
+                                {
+                                    steps
+                                            {
+                                                echo "Build is executed"
+                                            }
+                                }
+                    }
+        }
+    } else if (command == "deploy") {
+        pipeline {
+            agent any
+            stages
+                    {
+                        stage("Deploy")
+                                {
+                                    steps
+                                            {
+                                                echo "Deploy is executed"
+                                            }
+                                }
+                    }
+        }
+    } else {
+        pipeline {
+            agent any
+            stages
+                    {
+                        stage("Do Nothing")
+                                {
+                                    steps
+                                            {
+                                                echo "Doing nothing"
+                                            }
+                                }
+                    }
+        }
+    }
 }

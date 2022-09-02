fe93c09c99adc53d91c2576708d774ee2cf98589
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-09-02 16:47:15 +0300

    First

--- vars/helloWorldExternalv1.groovy
+++ vars/helloWorldExternalv1.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
    //01 - Carregar o Script
-   loadLinuxScript(name: "${scriptName}")
+   loadLinuxScript(name: "hello-world.sh")
    //02 - Executar o Script
-   sh "./${scriptName} ${config.name} ${config.dayOfWeek}"
+   sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
 }

81150278060687894b7051b4380530d0752c9ce0
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-09-05 09:48:34 +0300

    First

--- vars/helloWorldExternalv1.groovy
+++ vars/helloWorldExternalv1.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
    //01 - Carregar o Script
    loadLinuxScript(scriptName: "${scriptName}")
-   //02 - Executar o Script
+   //02 - Executar o Script - Criar essa entrada de parametro no Jenkins na pipeline
    sh "./${scriptName} ${config.name} ${config.dayOfWeek}"
 }
